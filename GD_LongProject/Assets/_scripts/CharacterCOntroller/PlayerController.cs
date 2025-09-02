7832796363ab896fe9ea29a41f076a3e6359be3c
Daynavos <dayna22vos@gmail.com>
2025-09-02 11:03:37 +0200

    super basic third person controller... no specific design

--- GD_LongProject/Assets/_scripts/CharacterCOntroller/PlayerController.cs
+++ GD_LongProject/Assets/_scripts/CharacterCOntroller/PlayerController.cs
@@ -1,68 +1,72 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    /// <summary>
-    /// This is basically bar for bar the documentation's implementation
-    /// </summary>
-    
-    //Input
-    public InputActionAsset  actionAsset;
+    [Header("Input")]
+    public InputActionAsset actionAsset;
     private InputAction _moveAction;
     private InputAction _jumpAction;
-    
+
     private Vector2 _moveInput;
 
-    private CharacterController _controller;
-    
-    private Vector3 _velocity;
-    
+    [Header("Movement")]
     [SerializeField] private float playerSpeed = 5.0f;
     [SerializeField] private float jumpHeight = 1.5f;
     [SerializeField] private float gravityValue = -9.81f;
-    private bool _grounded;
-    void OnEnable()
-    {
-        actionAsset.Enable();
-    }
 
-    void OnDisable()
-    {
-        actionAsset.Disable();
-    }
+    private CharacterController _controller;
+    private Vector3 _velocity;
+    private bool _grounded;
+
+    [Header("Camera")]
+    public Transform cameraTransform;
+    public bool faceMoveDirection = true;
+
+    void OnEnable() => actionAsset.Enable();
+    void OnDisable() => actionAsset.Disable();
 
     void Start()
     {
-        _controller =  gameObject.GetComponent<CharacterController>();
+        _controller = GetComponent<CharacterController>();
         _moveAction = actionAsset.FindAction("Move");
         _jumpAction = actionAsset.FindAction("Jump");
     }
 
     void Update()
     {
-        //Ground Logic
-        if (_grounded && _velocity.y < 0)
-        {
-            _velocity.y = 0f;
-        }
-        
-        //Read Input
-        _moveInput =  _moveAction.ReadValue<Vector2>();
-        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y);
-        move = Vector3.ClampMagnitude(move, 1f);
+        // Ground check
+        _grounded = _controller.isGrounded;
 
-        //Jump
-        if (_jumpAction.triggered && _controller.isGrounded)
+        // Read input
+        _moveInput = _moveAction.ReadValue<Vector2>();
+
+        // Camera-relative movement
+        Vector3 forward = cameraTransform.forward; forward.y = 0; forward.Normalize();
+        Vector3 right   = cameraTransform.right;   right.y = 0;   right.Normalize();
+
+        Vector3 move = forward * _moveInput.y + right * _moveInput.x;
+
+        if (faceMoveDirection && move.sqrMagnitude > 0.01f)
         {
-            _velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+            Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, Time.deltaTime * 10f);
         }
-        
-        //Apply Gravity
-        _velocity.y += gravityValue * Time.deltaTime;
-        
-        //Actually Move
-        Vector3 finalMove = (move * playerSpeed) + (_velocity.y * Vector3.up);
-        _controller.Move(finalMove * Time.deltaTime);
+
+        // Jump
+        if (_jumpAction.triggered && _grounded)
+        {
+            _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+        }
+
+        // Gravity
+        if (_grounded && _velocity.y < 0)
+            _velocity.y = -2f; // small downward force to stick to ground
+        else
+            _velocity.y += gravityValue * Time.deltaTime;
+
+        // Apply movement + gravity
+        _controller.Move((move * playerSpeed + _velocity) * Time.deltaTime);
     }
 }
