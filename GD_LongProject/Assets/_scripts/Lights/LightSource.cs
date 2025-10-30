7bd28036cfea6f178b54293ad17a50ce0101f3e4
Daynavos <dayna22vos@gmail.com>
2025-10-30 20:46:32 +0200

    Light cutoff logic for torch.
    Also started on shader cutoff logic for torch... getting there but a bit of a mismatch

--- GD_LongProject/Assets/_scripts/Lights/LightSource.cs
+++ GD_LongProject/Assets/_scripts/Lights/LightSource.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using System;
 
 public class LightSource : MonoBehaviour
-{ 
+{
+    private static readonly int FadeDistance = Shader.PropertyToID("_fadeDistance");
+
     [Header("Initial Value Data")]
     public lightProperties lightProperties;     // ScriptableObject with initial settings
     
@@ -71,10 +73,11 @@ public class LightSource : MonoBehaviour
         List<IChangeable> current = projectionType switch
         {
             lightProperties.ProjectionType.Lantern => LanternLook(transform.position, radialRangeOfLantern),
-            lightProperties.ProjectionType.Torch   => TorchLook(transform.position, forwardRangeOfTorch),
+            lightProperties.ProjectionType.Torch   => TorchLook(transform.position,  TorchActualDistance(forwardRangeOfTorch)), 
             _ => new List<IChangeable>()
         };
 
+        TorchShaderLogic(_visualizationMaterial);
         // Normalize null -> empty list (so we can safely Except/Any)
         current = current ?? new List<IChangeable>();
 
@@ -126,6 +129,27 @@ public class LightSource : MonoBehaviour
                 changeables.Add(changeable);
         }
         return changeables;
+    }
+    
+    private void TorchShaderLogic(Material material)
+    {
+        float fadeDistance = (forwardRangeOfTorch - TorchActualDistance(forwardRangeOfTorch))/forwardRangeOfTorch;
+        material.SetFloat(FadeDistance, fadeDistance  +1.8f);
+    }
+
+    private float TorchActualDistance(float forwardRange)
+    {
+        Debug.DrawRay(transform.position, transform.forward * forwardRange, Color.red);
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.SphereCast(ray, horizontalRangeOfTorch, out hit, forwardRange ))
+        {
+            if (hit.collider.CompareTag("Blue"))
+            {
+                return hit.distance;
+            }
+        }
+        return forwardRange;
     }
 
     // --- Lantern helpers ---
